24c6287b14ec4b7903ca25743a5785116d5c68ca
CompilationFail <2946316707@qq.com>
2021-06-08 15:58:52 +0800

    Site updated: 2021-06-08 15:58:51

--- js/userbg.js
+++ js/userbg.js
@@ -19,12 +19,13 @@ temp=obj.style.cssText;
 //console.log(temp);
 var pos=-1;
 changeBackground=function(){
-	pos=parseInt(Math.random()*a.length);
-	console.log(pos);
+	pos=parseInt(Math.random()*a.length); // 随机背景
+	//pos=(pos+1)%a.length; // 顺序背景
+	//console.log(pos);
 	obj.style.cssText=temp+"background:url(/images/"+a[pos]+") no-repeat fixed;"
 	+ "background-position:50% 50%;" // 图片位置：居中
 	+ "background-size: cover";
-	console.log(obj.style.cssText);
+	//console.log(obj.style.cssText);
 };
 //console.log(temp);
 setInterval(changeBackground,Timeinterval);
